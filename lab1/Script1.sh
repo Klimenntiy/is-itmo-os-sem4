6775b669c20489b1f8be7eecc1f0d8361d242c58
Klimenntiy <animilk04@gmail.com>
2025-02-20 21:55:01 +0300

    fix

--- lab1/Script1.sh
+++ lab1/Script1.sh
@@ -1,10 +1,18 @@
-#!/bin/sh
-arr=($1 $2 $3)
-let max=arr[1]
-for key in "${arr[@]}"
-do
-	if [ "$max" -lt "$key" ]; then
-		 max=$key
-	fi
-done
-echo $max
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Использование: $0 число1 число2 число3"
+    exit 1
+fi
+
+num1=$1
+num2=$2
+num3=$3
+
+if [ "$num1" -ge "$num2" ] && [ "$num1" -ge "$num3" ]; then
+    echo "Наибольшее число: $num1"
+elif [ "$num2" -ge "$num1" ] && [ "$num2" -ge "$num3" ]; then
+    echo "Наибольшее число: $num2"
+else
+    echo "Наибольшее число: $num3"
+fi
