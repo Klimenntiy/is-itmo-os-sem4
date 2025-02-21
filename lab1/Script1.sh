83d40022d99f3a2352f2619456fddcb17b51aa18
Klimenntiy <animilk04@gmail.com>
2025-02-21 20:13:22 +0300

    Edit

--- lab1/Script1.sh
+++ lab1/Script1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -ne 3 ]; then
-    echo "Использование: $0 число1 число2 число3"
+    echo "ERROR"
     exit 1
 fi
 
@@ -10,9 +10,9 @@ num2=$2
 num3=$3
 
 if [ "$num1" -ge "$num2" ] && [ "$num1" -ge "$num3" ]; then
-    echo "Наибольшее число: $num1"
+    echo "MAX_NUM: $num1"
 elif [ "$num2" -ge "$num1" ] && [ "$num2" -ge "$num3" ]; then
-    echo "Наибольшее число: $num2"
+    echo "MAX_NUM: $num2"
 else
-    echo "Наибольшее число: $num3"
+    echo "MAX_NUM: $num3"
 fi
