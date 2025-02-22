9a398b395ac6c9321291abf892b11b9609a2c063
Klimenntiy <animilk04@gmail.com>
2025-02-22 21:05:12 +0300

    Script6 add

--- lab1/Script6.sh
+++ lab1/Script6.sh
@@ -2,8 +2,8 @@
 
 > full.log
 
-awk '/WW/ { gsub(/WW/, "Warning:"); print }' /var/log/anaconda/X.log >> full.log
+awk '/WW/ { gsub(/WW/, "Warning:"); print }' /var/log/anaconda/journal.log >> full.log
 
-awk '/II/ { gsub(/II/, "Information:"); print }' /var/log/anaconda/X.log >> full.log
+awk '/II/ { gsub(/II/, "Information:"); print }' /var/log/anaconda/journal.log >> full.log
 
 cat full.log
