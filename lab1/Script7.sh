20b987e5b4f261fe7d78d28ce3cfe9985c73c2b0
Klimenntiy <animilk04@gmail.com>
2025-02-22 21:32:21 +0300

    Script7 add

--- lab1/Script7.sh
+++ lab1/Script7.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 touch emails.lst
 grep -E -o -R -i -h "\b\w+@gmail\S*" /etc | tr '\n' ','| sed 's/,$/\n/' >>emails.lst
+#sudo chmod o+r /etc/crypttab /etc/lvm/devices /etc/lvm/archive /etc/lvm/backup /etc/lvm/cache /etc/iscsi/iscsid.conf /etc/sssd /etc/pkcs11/rsyslog /etc/security/opasswd /etc/shadow- /etc/sos/cleaner /etc/audit /etc/pwd.lock /etc/gshadow- /etc/gshadow /etc/shadow
