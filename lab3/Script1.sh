67e2d144156a4edaeca7dbe181e9e98a622cba81
Klimenntiy <animilk04@gmail.com>
2025-03-24 01:43:49 +0300

    fix

--- lab3/Script1.sh
+++ lab3/Script1.sh
@@ -1,18 +1,5 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-HOME_DIR="/home/klimenntiy"
-TEST_DIR="$HOME_DIR/test"
-REPORT_FILE="$HOME_DIR/report"
+mkdir ~/test && echo "catalog test was created successfully" >> ~/report && touch ~/test/$(date +%Y-%m-%d_%H-%M-%S)
 
-mkdir -p "$TEST_DIR"
-
-echo "catalog test was created successfully" >> "$REPORT_FILE"
-
-TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-touch "$TEST_DIR/$TIMESTAMP"
-
-ping -c 1 www.net_nikogo.ru >/dev/null 2>&1
-
-echo "$(date +"%Y-%m-%d %H:%M:%S") Host unreachable error" >> "$REPORT_FILE"
-
-echo "Script execution started at $(date)" >> /home/klimenntiy/report
+ping net_nikogo.ru > /dev/null || echo "$(date +"%Y-%m-%d %H:%M:%S") Want full" >> ~/report
--- lab3/Script2.sh
+++ lab3/Script2.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-echo "sudo bash /home/klimenntiy/is-itmo-os-sem4/lab3/script1.sh" | at $(date -d "+20 seconds" +"%H:%M") 
-
-sleep 5
-
-tail -f /home/klimenntiy/report
+tail -f ~/report
