27c2583321f5365cbe1704f8cdbfc942b3744915
Klimenntiy <animilk04@gmail.com>
2025-03-24 02:14:36 +0300

    fix

--- lab3/Script1.sh
+++ lab3/Script1.sh
@@ -1,5 +1,21 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-mkdir /home/klimenntiy/test && echo "catalog test was created successfully" >> /home/klimenntiy/report && touch /home/klimenntiy/test/$(date +%Y-%m-%d_%H-%M-%S)
+REPORT_FILE="/home/klimenntiy/report"
+TEST_DIR="/home/klimenntiy/test"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
-ping net_nikogo.ru > /dev/null || echo "$(date +"%Y-%m-%d %H:%M:%S") Want full" >> ~/report
+mkdir -p "$TEST_DIR"
+
+echo "$(date +"%Y-%m-%d %H:%M:%S") - Directory $TEST_DIR created successfully." >> "$REPORT_FILE"
+
+touch "$TEST_DIR/$TIMESTAMP"
+echo "$(date +"%Y-%m-%d %H:%M:%S") - File $TIMESTAMP created in $TEST_DIR." >> "$REPORT_FILE"
+
+ping -c 1 www.net_nikogo.ru >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Host unreachable error." >> "$REPORT_FILE"
+else
+    echo "$(date +"%Y-%m-%d %H:%M:%S") - Host reachable." >> "$REPORT_FILE"
+fi
+
+sync
--- lab3/Script2.sh
+++ lab3/Script2.sh
@@ -1,3 +1,7 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-tail -f /home/klimenntiy/report
+echo "bash /home/klimenntiy/is-itmo-os-sem4/lab3/script1.sh" | at now + 20 seconds
+
+sleep 5
+
+tail -n 0 -f /home/klimenntiy/report
