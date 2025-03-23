a11b99ca66c2b8272bad433ea581382ef93989dd
Klimenntiy <animilk04@gmail.com>
2025-03-24 00:47:29 +0300

    fix

--- lab3/Script2.sh
+++ lab3/Script2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "bash /home/klimenntiy/is-itmo-os-sem4/lab3/script1.sh" | at now + 2 minutes
+echo "bash /home/klimenntiy/is-itmo-os-sem4/lab3/script1.sh" | at now + 20 seconds
+
+sleep 5
 
 tail -f /home/klimenntiy/report
