ef27f450bb979ebb710a2b89fa9c4562263610ec
Klimenntiy <animilk04@gmail.com>
2025-03-24 04:29:51 +0300

    fix

--- lab3/Script4.1.sh
+++ lab3/Script4.1.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 
 PID1=$(cat /tmp/pid1.txt)
-PID2=$(cat /tmp/pid2.txt)
 PID3=$(cat /tmp/pid3.txt)
 
 if ps -p $PID1 > /dev/null
 then
-    echo "Процесс с PID $PID1 найден."
+    echo "Процесс $PID1 найден. Ограничиваем CPU."
+
+    sudo taskset -cp 1 $PID1
 
     renice -n 19 -p $PID1
-    echo "Приоритет процесса с PID $PID1 снижен до 19."
-
-    sudo mkdir -p /sys/fs/cgroup/cpu/limitgroup
-    sudo bash -c "echo +cpu > /sys/fs/cgroup/cgroup.subtree_control"
-
-    sudo bash -c "echo 50000 > /sys/fs/cgroup/cpu/limitgroup/cpu.max"
-
-    sudo bash -c "echo $PID1 > /sys/fs/cgroup/cpu/limitgroup/cgroup.procs"
-
-    echo "Процесс с PID $PID1 теперь использует максимум 5% CPU."
+    echo "Процесс $PID1 теперь работает только на 1 ядре и с минимальным приоритетом."
 else
-    echo "Процесс с PID $PID1 не найден."
+    echo "Процесс $PID1 не найден."
 fi
 
 if ps -p $PID3 > /dev/null
 then
     kill $PID3
-    echo "Процесс с PID $PID3 завершен."
+    echo "Процесс $PID3 завершен."
 else
-    echo "Процесс с PID $PID3 не найден."
+    echo "Процесс $PID3 не найден."
 fi
