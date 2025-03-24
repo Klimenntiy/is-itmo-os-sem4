3719f0f3416f8b9dbfaab28fae88969db9f0e15f
Klimenntiy <animilk04@gmail.com>
2025-03-24 04:15:54 +0300

    fix

--- lab3/Script4.1.sh
+++ lab3/Script4.1.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+PID1=$(cat /tmp/pid1.txt)
+PID2=$(cat /tmp/pid2.txt)
+PID3=$(cat /tmp/pid3.txt)
+
 if ps -p $PID1 > /dev/null
 then
     echo "Процесс с PID $PID1 найден."
+    renice -n 10 -p $PID1
+    echo "Приоритет процесса с PID $PID1 снижен до 10."
 else
     echo "Процесс с PID $PID1 не найден."
 fi
-
-renice -n 10 -p $PID1
-echo "Приоритет процесса с PID $PID1 снижен до 10."
 
 if ps -p $PID3 > /dev/null
 then
--- lab3/Script4.sh
+++ lab3/Script4.sh
@@ -6,16 +6,19 @@ do
     echo \$((2 * 2)) > /dev/null
 done" > /tmp/multiply_numbers.sh
 
-
 chmod +x /tmp/multiply_numbers.sh
 
 nice -n 10 /tmp/multiply_numbers.sh &  
-PID1=$!             
-
-nice -n 10 /tmp/multiply_numbers.sh & 
-PID2=$!             
+PID1=$!
 
 nice -n 10 /tmp/multiply_numbers.sh &  
-PID3=$!            
+PID2=$!
 
-echo "Процессы запущены с PID: $PID1, $PID2, $PID3"
+nice -n 10 /tmp/multiply_numbers.sh &  
+PID3=$!
+
+echo "$PID1" > /tmp/pid1.txt
+echo "$PID2" > /tmp/pid2.txt
+echo "$PID3" > /tmp/pid3.txt
+
+echo "Процессы запущены: PID1=$PID1, PID2=$PID2, PID3=$PID3"
