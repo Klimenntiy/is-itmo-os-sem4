47a94b615e93ebf2dfccb4c4908d60df66740f18
Klimenntiy <animilk04@gmail.com>
2025-03-24 04:25:09 +0300

    fix

--- lab3/Script4.1.sh
+++ lab3/Script4.1.sh
@@ -7,8 +7,18 @@ PID3=$(cat /tmp/pid3.txt)
 if ps -p $PID1 > /dev/null
 then
     echo "Процесс с PID $PID1 найден."
+
     renice -n 19 -p $PID1
-    echo "Приоритет процесса с PID $PID1 снижен до 10."
+    echo "Приоритет процесса с PID $PID1 снижен до 19."
+
+    sudo mkdir -p /sys/fs/cgroup/cpu/limitgroup
+    sudo bash -c "echo +cpu > /sys/fs/cgroup/cgroup.subtree_control"
+
+    sudo bash -c "echo 50000 > /sys/fs/cgroup/cpu/limitgroup/cpu.max"
+
+    sudo bash -c "echo $PID1 > /sys/fs/cgroup/cpu/limitgroup/cgroup.procs"
+
+    echo "Процесс с PID $PID1 теперь использует максимум 5% CPU."
 else
     echo "Процесс с PID $PID1 не найден."
 fi
