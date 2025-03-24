bdd96af2832f5e09c926c499c0b6df4cbfe90b8c
Klimenntiy <animilk04@gmail.com>
2025-03-24 03:09:29 +0300

    fix

--- lab3/Script4.1.sh
+++ lab3/Script4.1.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+if ps -p $PID1 > /dev/null
+then
+    echo "Процесс с PID $PID1 найден."
+else
+    echo "Процесс с PID $PID1 не найден."
+fi
+
 renice -n 10 -p $PID1
-echo "Процесс с PID $PID1 ограничен на 10% процессора."
+echo "Приоритет процесса с PID $PID1 снижен до 10."
 
-kill $PID3
-echo "Процесс с PID $PID3 завершен."
-
+if ps -p $PID3 > /dev/null
+then
+    kill $PID3
+    echo "Процесс с PID $PID3 завершен."
+else
+    echo "Процесс с PID $PID3 не найден."
+fi
